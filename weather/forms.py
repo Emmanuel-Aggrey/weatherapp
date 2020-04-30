aaba71235c7c14c9c0cc5059bfa062b074b58f9a
Emmanuel-Aggrey <aggrey.en@gmail.com>
2020-04-30 02:04:05 +0000

    first commit

---
+++ weather/forms.py
@@ -0,0 +1,12 @@
+from django.forms import ModelForm, TextInput
+from .models import city
+from django import  forms
+class CityForm(ModelForm):
+    # name = forms.forms.MultipleChoiceField()
+
+    class Meta:
+        model = city
+        fields = ['name']
+        widgets = {
+            'name':TextInput(attrs={'class' : 'input','placholder' : 'City Name'})
+        }
